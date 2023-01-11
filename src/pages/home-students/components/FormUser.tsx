2bba7183662cba0ee3cd354bddef7480428be2a9
Felipe Segovia R <fsegovia@lisit.cl>
2023-01-11 14:16:24 -0300

    Se eliminan variables sin uso

--- src/pages/home-students/components/FormUser.tsx
+++ src/pages/home-students/components/FormUser.tsx
@@ -33,10 +33,7 @@ type PropsFormUser = {
 
 export const FormUser = (props: PropsFormUser) => {
   const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors }
+    handleSubmit
   } = useForm<FormType>();
 
   const onSubmit: SubmitHandler<FormType> = (data) => {
@@ -47,69 +44,69 @@ export const FormUser = (props: PropsFormUser) => {
   };
 
   return (
-    <form method="post" className="mt-5" onSubmit={handleSubmit(onSubmit)}>
+    <form method='post' className='mt-5' onSubmit={handleSubmit(onSubmit)}>
       <label> Datos personales</label>
-      <div className="flex">
-        <input type="text" placeholder="Nombre" disabled />
-        <input type="text" placeholder="Apellido Paterno" />
-        <input type="text" placeholder="Apellido Materno" />
-        <input type="text" placeholder="Tipo de Identificación" />
+      <div className='flex'>
+        <input type='text' placeholder='Nombre' disabled />
+        <input type='text' placeholder='Apellido Paterno' />
+        <input type='text' placeholder='Apellido Materno' />
+        <input type='text' placeholder='Tipo de Identificación' />
       </div>
-      <div className="grid grid-cols-3">
-        <input type="text" placeholder="Número de identificación" />
-        <input type="text" placeholder="Código del país" />
-        <input type="text" placeholder="Número de teléfono" />
+      <div className='grid grid-cols-3'>
+        <input type='text' placeholder='Número de identificación' />
+        <input type='text' placeholder='Código del país' />
+        <input type='text' placeholder='Número de teléfono' />
       </div>
-      <div className="grid grid-cols-3">
+      <div className='grid grid-cols-3'>
         <input
-          className="col-span-2"
-          type="email"
-          placeholder="Correo electrónico"
+          className='col-span-2'
+          type='email'
+          placeholder='Correo electrónico'
         />
-        <input type="text" placeholder="Fecha de nacimiento" />
+        <input type='text' placeholder='Fecha de nacimiento' />
       </div>
-      <div className="flex">
-        <input type="text" placeholder="Dirección" />
-        <input type="text" placeholder="Ciudad" />
-        <input type="text" placeholder="País de residencia" />
-        <input type="text" placeholder="Nacionalidad" />
+      <div className='flex'>
+        <input type='text' placeholder='Dirección' />
+        <input type='text' placeholder='Ciudad' />
+        <input type='text' placeholder='País de residencia' />
+        <input type='text' placeholder='Nacionalidad' />
       </div>
-      <div className="mt-3">
+      <div className='mt-3'>
         <label>Educación</label>
-        <div className="grid grid-cols-2">
-          <input type="text" placeholder="Nivel educacional" />
-          <input type="text" placeholder="Nivel de ingles" />
+        <div className='grid grid-cols-2'>
+          <input type='text' placeholder='Nivel educacional' />
+          <input type='text' placeholder='Nivel de ingles' />
         </div>
-        <div className="grid grid-cols-2">
-          <input type="text" placeholder="¿Posee conocimientos previos?" />
-          <input type="text" placeholder="¿Cuales?" />
+        <div className='grid grid-cols-2'>
+          <input type='text' placeholder='¿Posee conocimientos previos?' />
+          <input type='text' placeholder='¿Cuales?' />
         </div>
       </div>
-      <div className="mt-3">
+      <div className='mt-3'>
         <label>Empleabilidad</label>
-        <div className="grid grid-cols-2">
-          <input type="text" placeholder="Situación laboral" />
-          <input type="text" placeholder="Link Linkedin" />
+        <div className='grid grid-cols-2'>
+          <input type='text' placeholder='Situación laboral' />
+          <input type='text' placeholder='Link Linkedin' />
         </div>
-        <div className="grid grid-cols-3">
-          <input type="text" placeholder="Empresa donde trabaja" />
-          <input type="text" placeholder="Cargo" />
-          <input type="text" placeholder="Renta Actual" />
+        <div className='grid grid-cols-3'>
+          <input type='text' placeholder='Empresa donde trabaja' />
+          <input type='text' placeholder='Cargo' />
+          <input type='text' placeholder='Renta Actual' />
         </div>
       </div>
-      <div className="mt-3">
+      <div className='mt-3'>
         <label>Registro</label>
-        <div className="grid grid-cols-3">
+        <div className='grid grid-cols-3'>
           <input
-            type="text"
-            className="col-span-2"
-            placeholder="Carrera a la cual te estas matriculando"
+            type='text'
+            className='col-span-2'
+            placeholder='Carrera a la cual te estas matriculando'
           />
-          <input type="text" placeholder="Generación a la que matrícula" />
+          <input type='text' placeholder='Generación a la que matrícula' />
         </div>
       </div>
-      <div className="flex justify-end mt-5">
-        <button className="btn" type="submit">
+      <div className='flex justify-end mt-5'>
+        <button className='btn' type='submit'>
           Siguiente
         </button>
       </div>
