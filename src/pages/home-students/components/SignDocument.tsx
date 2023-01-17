d26bffd80ed1968d1cb78e28bf2dd4b872700a7e
Felipe Segovia Rodríguez <fsegovia@lisit.cl>
2023-01-16 21:07:18 -0300

    Se actualiza la forma de pago

--- src/pages/home-students/components/SignDocument.tsx
+++ src/pages/home-students/components/SignDocument.tsx
@@ -6,19 +6,26 @@ type PropsFormUser = {
 };
 
 export const SignDocument = (props: PropsFormUser) => {
-
   const nextStep = () => {
     props.setCurrentStep(props.currentStep + 1);
   };
 
-  return <div className={'container-firma'}>
-    <h1 className={'text-5xl font-bold w-3/5 text-center'}>Firma acuerdo vía Docusign</h1>
-    {/* Falta la imagen de firma */}
-    <img src={require('../../../assets/images/img_firma.jpeg')} className={'w-3/5 py-5'} alt={'firma-docusign'} />
-    <div className='flex justify-end mt-5'>
-      <button className='btn' type='submit' onClick={() => nextStep()}>
-        Firmar ahora
-      </button>
+  return (
+    <div className={'container-firma'}>
+      <h1 className={'text-5xl font-bold w-3/5 text-center'}>
+        Firma acuerdo vía Docusign
+      </h1>
+      {/* Falta la imagen de firma */}
+      <img
+        src={require('../../../assets/images/img_firma.png')}
+        className={'w-3/5 py-5'}
+        alt={'firma-docusign'}
+      />
+      <div className="flex justify-end mt-5">
+        <button className="btn" type="submit" onClick={() => nextStep()}>
+          Firmar ahora
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 };
