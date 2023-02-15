b9b4661cbe306224148f9dd88b00508cd19af5cb
Felipe Segovia R <fsegovia@lisit.cl>
2023-02-15 16:25:30 -0300

    Se prueba desafio latam

--- src/pages/admin/section-payment/components/TablePrices.tsx
+++ src/pages/admin/section-payment/components/TablePrices.tsx
@@ -49,62 +49,66 @@ export const TablePrices = () => {
   return (
     <PaymentFormProvider {...initialValue}>
       <>
-        <DataTable
-          value={listPrices}
-          responsiveLayout="stack"
-          breakpoint="960px"
-          dataKey="id"
-          rows={15}
-          filters={filters}
-          loading={loading}
-          filterDisplay="row"
-          paginator
-          className="shadow-lg shadow-gray-500/30"
-          header={<HeaderTable />}
-        >
-          <Column field="id" header={'Id'} sortable></Column>
-          <Column
-            field="name"
-            filter
-            filterPlaceholder={'Buscar por tabla'}
-            header={'Tabla de precios'}
-            sortable
-          ></Column>
-          <Column
-            field={'career.description'}
-            filter
-            filterPlaceholder={'Buscar por programa'}
-            header={'Programa'}
-            sortable
-          ></Column>
-          <Column
-            field="value"
-            header={'Valor de referencia'}
-            sortable
-          ></Column>
-          <Column
-            field="advance_discount"
-            header={'Dcto. Cuotas'}
-            sortable
-          ></Column>
-          <Column
-            field="free_discount"
-            header={'Dscto. Anticipado'}
-            sortable
-          ></Column>
-          <Column field="tuition" header={'Matricula'} sortable></Column>
-          <Column
-            field="comments"
-            header={'Motivo descuento'}
-            sortable
-          ></Column>
-          <Column
-            body={actionEdit}
-            exportable={false}
-            style={{ minWidth: '8rem' }}
-          ></Column>
-        </DataTable>
-        <DialogTablePricing />
+        {!loading && (
+          <>
+            <DataTable
+              value={listPrices}
+              responsiveLayout="stack"
+              breakpoint="960px"
+              dataKey="id"
+              rows={15}
+              filters={filters}
+              loading={loading}
+              filterDisplay="row"
+              paginator
+              className="shadow-lg shadow-gray-500/30"
+              header={<HeaderTable />}
+            >
+              <Column field="id" header={'Id'} sortable></Column>
+              <Column
+                field="name"
+                filter
+                filterPlaceholder={'Buscar por tabla'}
+                header={'Tabla de precios'}
+                sortable
+              ></Column>
+              <Column
+                field={'career.description'}
+                filter
+                filterPlaceholder={'Buscar por programa'}
+                header={'Programa'}
+                sortable
+              ></Column>
+              <Column
+                field="value"
+                header={'Valor de referencia'}
+                sortable
+              ></Column>
+              <Column
+                field="advance_discount"
+                header={'Dcto. Cuotas'}
+                sortable
+              ></Column>
+              <Column
+                field="free_discount"
+                header={'Dscto. Anticipado'}
+                sortable
+              ></Column>
+              <Column field="tuition" header={'Matricula'} sortable></Column>
+              <Column
+                field="comments"
+                header={'Motivo descuento'}
+                sortable
+              ></Column>
+              <Column
+                body={actionEdit}
+                exportable={false}
+                style={{ minWidth: '8rem' }}
+              ></Column>
+            </DataTable>
+            <DialogTablePricing />
+          </>
+        )}
         {rowSelected && <DialogEditPricing {...rowSelected} />}
       </>
     </PaymentFormProvider>
