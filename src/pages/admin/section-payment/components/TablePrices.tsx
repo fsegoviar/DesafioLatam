e4c2e514f4cb0ea283edada4e7db6f503b187563
Felipe Segovia R <fsegovia@lisit.cl>
2023-02-15 16:34:31 -0300

    Se agrega el Axios GET directamente en la tabla de precios

--- src/pages/admin/section-payment/components/TablePrices.tsx
+++ src/pages/admin/section-payment/components/TablePrices.tsx
@@ -4,18 +4,46 @@ import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { DialogTablePricing } from './DialogTablePricing';
 import { HeaderTable } from './HeaderTable';
-import { GetPricesTable } from '../../../../services/Prices';
 import { PaymentType } from '../../../../interfaces';
 import { useDialogEditPriceHook } from '../context/TableContext';
 import { DialogEditPricing } from './DialogEditPricing';
+import {
+  PaymentFormProvider,
+  initialValue
+} from '../context/PaymentFormContext';
 import { UseFormPayment } from '../hooks/useFormPayment';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const TablePrices = () => {
-  const { listPrices, loading } = GetPricesTable();
+  // const { listPrices, loading } = GetPricesTable();
   const { openDialogEdit } = useDialogEditPriceHook();
   const { updateForm } = UseFormPayment();
   const [rowSelected, setRowSelected] = useState<PaymentType>();
+  const [listPrices, setListPrices] = useState<PaymentType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_BACKEND}/prices`,
+        {
+          headers: {
+            Accept: 'application/json'
+          }
+        }
+      );
+      setListPrices(response.data);
+    } catch (error) {
+      console.log('Error =>', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const filters = {
     nombre: {
@@ -43,56 +71,66 @@ export const TablePrices = () => {
   };
 
   return (
-    <>
-      <DataTable
-        value={listPrices}
-        responsiveLayout="stack"
-        breakpoint="960px"
-        dataKey="id"
-        rows={15}
-        filters={filters}
-        loading={loading}
-        filterDisplay="row"
-        paginator
-        className="shadow-lg shadow-gray-500/30"
-        header={<HeaderTable />}
-      >
-        <Column field="id" header={'Id'} sortable></Column>
-        <Column
-          field="name"
-          filter
-          filterPlaceholder={'Buscar por tabla'}
-          header={'Tabla de precios'}
-          sortable
-        ></Column>
-        <Column
-          field={'career.description'}
-          filter
-          filterPlaceholder={'Buscar por programa'}
-          header={'Programa'}
-          sortable
-        ></Column>
-        <Column field="value" header={'Valor de referencia'} sortable></Column>
-        <Column
-          field="advance_discount"
-          header={'Dcto. Cuotas'}
-          sortable
-        ></Column>
-        <Column
-          field="free_discount"
-          header={'Dscto. Anticipado'}
-          sortable
-        ></Column>
-        <Column field="tuition" header={'Matricula'} sortable></Column>
-        <Column field="comments" header={'Motivo descuento'} sortable></Column>
-        <Column
-          body={actionEdit}
-          exportable={false}
-          style={{ minWidth: '8rem' }}
-        ></Column>
-      </DataTable>
-      <DialogTablePricing />
-      {rowSelected && <DialogEditPricing {...rowSelected} />}
-    </>
+    <PaymentFormProvider {...initialValue}>
+      <>
+        <DataTable
+          value={listPrices}
+          responsiveLayout="stack"
+          breakpoint="960px"
+          dataKey="id"
+          rows={15}
+          filters={filters}
+          loading={loading}
+          filterDisplay="row"
+          paginator
+          className="shadow-lg shadow-gray-500/30"
+          header={<HeaderTable />}
+        >
+          <Column field="id" header={'Id'} sortable></Column>
+          <Column
+            field="name"
+            filter
+            filterPlaceholder={'Buscar por tabla'}
+            header={'Tabla de precios'}
+            sortable
+          ></Column>
+          <Column
+            field={'career.description'}
+            filter
+            filterPlaceholder={'Buscar por programa'}
+            header={'Programa'}
+            sortable
+          ></Column>
+          <Column
+            field="value"
+            header={'Valor de referencia'}
+            sortable
+          ></Column>
+          <Column
+            field="advance_discount"
+            header={'Dcto. Cuotas'}
+            sortable
+          ></Column>
+          <Column
+            field="free_discount"
+            header={'Dscto. Anticipado'}
+            sortable
+          ></Column>
+          <Column field="tuition" header={'Matricula'} sortable></Column>
+          <Column
+            field="comments"
+            header={'Motivo descuento'}
+            sortable
+          ></Column>
+          <Column
+            body={actionEdit}
+            exportable={false}
+            style={{ minWidth: '8rem' }}
+          ></Column>
+        </DataTable>
+        <DialogTablePricing />
+        {rowSelected && <DialogEditPricing {...rowSelected} />}
+      </>
+    </PaymentFormProvider>
   );
 };
