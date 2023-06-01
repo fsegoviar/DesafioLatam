1da39736a47b52d77def312bcc2cbc8faf082e02
Felipe Segovia Rodríguez <felipe.segovia.rod@gmail.com>
2023-06-01 10:38:48 -0400

    Se agrega label de restriccion de extensiones y se agrga el accept a los input

--- src/pages/home-students/components/form-carrera/FormAval.tsx
+++ src/pages/home-students/components/form-carrera/FormAval.tsx
@@ -145,7 +145,6 @@ export const FormAval = (props: PropsFormUser) => {
   };
 
   const verifySizeFiles = (files: any, type: string) => {
-    console.log('Hola=>', files);
     const MAX_SIZE = 20_000_000; // 20MB
     let countSize = 0;
 
@@ -319,10 +318,14 @@ export const FormAval = (props: PropsFormUser) => {
         <div className="grid gap-4 grid-cols-4 mt-5">
           <div className="col-span-4 flex flex-col">
             <label>Liquidaciones de Renta</label>
+            <span className="text-gray-500 mt-2">
+              Extensiones permitidas .pdf, .png, .jpg, .jpeg, .docx
+            </span>
             <input
               className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
               type="file"
               id="formFileMultiple"
+              accept="image/png, image/jpg, image/jpeg,application/msword, application/pdf"
               multiple
               disabled={isBilling}
               {...register('liquidaciones', {
@@ -367,10 +370,14 @@ export const FormAval = (props: PropsFormUser) => {
                 </label>
               </div>
             </div>
+            <span className="text-gray-500 mt-2">
+              Extensiones permitidas .pdf, .png, .jpg, .jpeg, .docx
+            </span>
             <input
               className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
               type="file"
               id="formFileMultiple"
+              accept="image/png, image/jpg, image/jpeg,application/msword, application/pdf"
               multiple
               disabled={isBilling || isEnableHistorical}
               {...register('historiales', {
