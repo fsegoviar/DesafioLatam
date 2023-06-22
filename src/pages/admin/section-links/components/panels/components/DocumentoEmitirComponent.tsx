5adee8e0098edf121fcf9f97d79ca2202c3e0f59
Felipe Segovia Rodríguez <felipe.segovia.rod@gmail.com>
2023-06-22 05:33:41 -0400

    se ajusta el tipo de pago y la forma de pago

--- src/pages/admin/section-links/components/panels/components/DocumentoEmitirComponent.tsx
+++ src/pages/admin/section-links/components/panels/components/DocumentoEmitirComponent.tsx
@@ -10,32 +10,56 @@ export const DocumentoEmitirComponent = (props: PropsPanel) => {
       <div className="grid gap-4 grid-cols-8 ">
         <div className="col-span-4 flex flex-col justify-center h-100">
           <label>
-            Tipo: <b>{props.dataUser.billing? 'Factura' : 'Boleta'}</b>
+            Tipo: <b>{props.dataUser.billing.document_type.description}</b>
           </label>
         </div>
         <div className="col-span-4 flex flex-col">
           <label>Razón social</label>
-          <input type="text" value={props.dataUser.billing?.business_name ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.business_name ?? ''}
+            disabled
+          />
         </div>
         <div className="col-span-4 flex flex-col">
           <label>Rut de la empresa</label>
-          <input type="text" value={props.dataUser.billing?.dni ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.dni ?? ''}
+            disabled
+          />
         </div>
         <div className="col-span-4 flex flex-col">
           <label>Giro</label>
-          <input type="text" value={props.dataUser.billing?.business_line ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.business_line ?? ''}
+            disabled
+          />
         </div>
         <div className="col-span-8 flex flex-col">
           <label>Dirección</label>
-          <input type="text" value={props.dataUser.billing?.address ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.address ?? ''}
+            disabled
+          />
         </div>
         <div className="col-span-4 flex flex-col">
           <label>Correo empresa</label>
-          <input type="text" value={props.dataUser.billing?.email ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.email ?? ''}
+            disabled
+          />
         </div>
         <div className="col-span-4 flex flex-col">
           <label>Teléfono empresa</label>
-          <input type="text" value={props.dataUser.billing?.phone ?? ''} disabled />
+          <input
+            type="text"
+            value={props.dataUser.billing?.phone ?? ''}
+            disabled
+          />
         </div>
       </div>
     </>
--- src/pages/admin/section-links/components/panels/components/FormaPagoComponent.tsx
+++ src/pages/admin/section-links/components/panels/components/FormaPagoComponent.tsx
@@ -10,7 +10,8 @@ export const FormaPagoComponent = (props: PropsPanel) => {
       <div className="grid gap-4 grid-cols-8 ">
         <div className="col-span-8 flex flex-col justify-center items-center h-100 w-100">
           <label>
-            Forma de pago: <b>Pago en Cuotas</b>
+            Forma de pago:{' '}
+            <b>{props.dataUser.purchase?.payment_method?.description ?? ''}</b>
           </label>
         </div>
       </div>
